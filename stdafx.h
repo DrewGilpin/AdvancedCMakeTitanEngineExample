3891165cd6c5909ebe86b5754e129fc73644538f
Drew Gilpin <drew.gilpin@gmail.com>
2025-05-30 14:49:20 -0400

    Pre-include networking headers

--- stdafx.h
+++ stdafx.h
@@ -1,2 +1,11 @@
-﻿#pragma once
+#pragma once
+#ifdef _WIN32
+#  define WIN32_LEAN_AND_MEAN
+#  include <Windows.h>
+#else
+#  include <sys/types.h>
+#  include <sys/socket.h>
+#  include <netinet/in.h>
+#  include <arpa/inet.h>
+#endif
 #include "Engine.h"
